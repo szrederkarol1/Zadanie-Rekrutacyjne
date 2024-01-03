1645d8c1548d7e5283ff249ad37cb36960b1e708
Karol Szreder <szrederkarol1@gmail.com>
2024-01-03 22:45:09 +0100

    Create navigation in first section

--- public/app.js
+++ public/app.js
@@ -24,35 +24,7 @@ searchInput.addEventListener("blur", () => {
 });
 
 //CAROUSEL
-document.querySelectorAll(".carousel").forEach((carousel) => {
-  const items = carousel.querySelectorAll(".carousel__item");
-  const buttonsCarousel = Array.from(items, () => {
-    return `<span class="carousel__button"></span>`;
-  });
-  const buttonsHtml = buttonsCarousel.join(""); // Utworzenie HTML z przyciskami
 
-  carousel.insertAdjacentHTML(
-    "beforeend",
-    `<div class="carousel__nav">${buttonsHtml}</div>`
-  );
-
-  const buttons = carousel.querySelectorAll(".carousel__button");
-
-  buttons.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      items.forEach((item) =>
-        item.classList.remove("carousel__item--selected")
-      );
-      buttons.forEach((btn) =>
-        btn.classList.remove("carousel__button_selected")
-      );
-      items[i].classList.add("carousel__item--selected");
-      button.classList.add("carousel__button_selected");
-    });
-  });
-  items[0].classList.add("carousel__item--selected");
-  buttons[0].classList.add("carousel__button_selected");
-});
 
 //HAMBURGER MENU
 const hamburger = document.querySelector(".hamburger-menu");
