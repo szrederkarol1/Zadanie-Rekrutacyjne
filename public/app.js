2577cc2d5d171c54c9e30e97a52aade389c07950
Karol Szreder <szrederkarol1@gmail.com>
2024-01-03 22:18:00 +0100

    create cross on hamburger

--- public/app.js
+++ public/app.js
@@ -54,19 +54,28 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
   buttons[0].classList.add("carousel__button_selected");
 });
 
+//HAMBURGER MENU
 const hamburger = document.querySelector(".hamburger-menu");
 const menu = document.querySelector(".collapse");
 const header = document.querySelector(".navbar");
+const topline = document.querySelector(".topline");
+const midline = document.querySelector(".midline");
+const bottomline = document.querySelector(".bottomline");
 
 hamburger.addEventListener("click", function () {
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
-  }
-  if (header.style.height === "100vh") {
-    header.style.height = "75px";
-  } else {
-    header.style.height = "100vh";
-  }
+  const isMenuVisible = menu.style.display === "flex";
+
+  menu.style.display = isMenuVisible ? "none" : "flex";
+  midline.style.display = isMenuVisible ? "flex" : "none";
+  topline.style.transform = isMenuVisible ? "rotate(0deg)" : "rotate(-45deg)";
+  bottomline.style.transform = isMenuVisible ? "rotate(0deg)" : "rotate(45deg)";
+  hamburger.style.position = isMenuVisible ? "static" : "absolute";
+  bottomline.style.position = topline.style.position = isMenuVisible
+    ? "static"
+    : "relative";
+  bottomline.style.left = topline.style.left = isMenuVisible ? "auto" : "225px";
+  topline.style.top = isMenuVisible ? "auto" : "15px";
+  hamburger.style.justifyContent = isMenuVisible ? "auto" : "space-around";
+
+  header.style.height = isMenuVisible ? "75px" : "100vh";
 });
